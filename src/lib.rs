2c13cc4200225fa32f419f85ed06f4267ffcde01
Riccardo Casatta <riccardo@casatta.it>
2018-09-18 14:56:59 -0700

    other api endpoints

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,7 @@ extern crate stderrlog;
 extern crate sysconf;
 extern crate time;
 extern crate tiny_http;
+extern crate url;
 extern crate hyper;
 
 #[macro_use]
@@ -34,6 +35,8 @@ extern crate log;
 extern crate serde_derive;
 #[macro_use]
 extern crate serde_json;
+
+
 
 pub mod app;
 pub mod bulk;
